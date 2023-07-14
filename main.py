2c9dbaf89f4ba83d1904177256a416cc944d0f7d
KaranManvani <37587455+KaranManvani@users.noreply.github.com>
2023-07-15 00:21:05 +0530

    Added timezone specifications and updated requirements.
    Timezone for APScheduler jobs has now been specified to Indian Standard Time 'IST = UTC + 5:30' . Added 'lxml' in requirements.txt file.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from website.schedules import insert_into_db
 
 scheduler = APScheduler()
 
-scheduler.add_job(id = 'task1', func = insert_into_db, args = ['s4d_international'], trigger = 'cron', minute = 0, hour = 4)
-scheduler.add_job(id = 'task2', func = insert_into_db, args = ['b4s_international_ao'], trigger = 'cron', minute = 30, hour = 4)
-scheduler.add_job(id = 'task3', func = insert_into_db, args = ['b4s_international'], trigger = 'cron', minute = 0, hour = 5)
-scheduler.add_job(id = 'task4', func = insert_into_db, args = ['b4s_girls_ao'], trigger = 'cron', minute = 30, hour = 5)
-scheduler.add_job(id = 'task5', func = insert_into_db, args = ['b4s_girls'], trigger = 'cron', minute = 0, hour = 6)
-scheduler.add_job(id = 'task6', func = insert_into_db, args = ['b4s_gov_uc'], trigger = 'cron', minute = 30, hour = 6)
+scheduler.add_job(id = 'task1', func = insert_into_db, args = ['s4d_international'], trigger = 'cron', minute = 0, hour = 4, timezone = 'IST')
+scheduler.add_job(id = 'task2', func = insert_into_db, args = ['b4s_international_ao'], trigger = 'cron', minute = 30, hour = 4, timezone = 'IST')
+scheduler.add_job(id = 'task3', func = insert_into_db, args = ['b4s_international'], trigger = 'cron', minute = 0, hour = 5, timezone = 'IST')
+scheduler.add_job(id = 'task4', func = insert_into_db, args = ['b4s_girls_ao'], trigger = 'cron', minute = 30, hour = 5, timezone = 'IST')
+scheduler.add_job(id = 'task5', func = insert_into_db, args = ['b4s_girls'], trigger = 'cron', minute = 0, hour = 6, timezone = 'IST')
+scheduler.add_job(id = 'task6', func = insert_into_db, args = ['b4s_gov_uc'], trigger = 'cron', minute = 30, hour = 6, timezone = 'IST')
 scheduler.init_app(app)
 scheduler.start()
 
